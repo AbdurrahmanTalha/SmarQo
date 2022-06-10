d64f553212fe82fbacb09adeb9c49281b6c69eae
AfnanFerdousi <afnanferdousi550@gmail.com>
2022-06-10 20:56:07 +0600

    details profile margin fixed

---
+++ src/components/helper/commonBtn/CommonBtn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommonBtn = () => {
+    return (
+        <div>
+            <button className="btn py-[11px] lg:md:w-[241px] w-[236px] bg-[#0C0C0C] lg:md:mt-[38px] mt-[45px] lg:md:mb-[19px] mb-[193px] lg:md:text-[15px] text-[14px] text-[#ffffff] rounded text-center">Change my password</button>
+        </div>
+    );
+};
+
+export default CommonBtn;
