0fe3b597b20de990bc4a31ac8ee22665c0c36b13
AbdurrahmanTalha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2022-06-18 22:53:34 +0600

    changed button to input field

--- src/components/Chat/Contacts.js
+++ src/components/Chat/Contacts.js
@@ -4,10 +4,10 @@ import ChairGuy from "../../asserts/Mask Group 6.png"
 
 const Contacts = () => {
     return (
-        <div className='lg:md:px-[8px] px-[4px] lg:md:w-[300px] xl:w-[350px] mx-auto mt-[15px] w-screen'>
+        <div className='lg:md:px-[0px] px-[4px] lg:md:w-[300px] xl:w-[350px] mx-auto mt-[15px] w-screen'>
             <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
                     lg:md:mb-[9.9px] mx-auto bg-[#f5ecec]" />
-            <div className="flex w-full p-1">
+            <div className="flex w-full p-1 ml-[12px]">
                 <img src={ChairGuy} alt="" className="lg:md:w-[54px] lg:md:h-[54px] w-[51px] h-[51px] rounded-full lg:md:mr-[13px] mr-[12px]" />
                 <div>
                     <h2 className="lg:md:text-[16px] text-[15px] font-medium lg:md:mb-[4px]">Name</h2>
@@ -17,9 +17,7 @@ const Contacts = () => {
             <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
                     lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:mt-[12px]" />
 
-            <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
-                    lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:block hidden" />
-            <div className="flex w-full p-1">
+            <div className="flex w-full p-1 ml-[12px]">
                 <img src={ChairGuy} alt="" className="lg:md:w-[54px] lg:md:h-[54px] w-[51px] h-[51px] rounded-full lg:md:mr-[13px] mr-[12px]" />
                 <div>
                     <h2 className="lg:md:text-[16px] text-[15px] font-medium lg:md:mb-[4px]">Name</h2>
@@ -29,9 +27,7 @@ const Contacts = () => {
             <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
                     lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:mt-[12px]" />
 
-            <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
-                    lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:block hidden" />
-            <div className="flex w-full p-1">
+            <div className="flex w-full p-1 ml-[12px]">
                 <img src={ChairGuy} alt="" className="lg:md:w-[54px] lg:md:h-[54px] w-[51px] h-[51px] rounded-full lg:md:mr-[13px] mr-[12px]" />
                 <div>
                     <h2 className="lg:md:text-[16px] text-[15px] font-medium lg:md:mb-[4px]">Name</h2>
@@ -41,9 +37,7 @@ const Contacts = () => {
             <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
                     lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:mt-[12px]" />
 
-            <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
-                    lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:block hidden" />
-            <div className="flex w-full p-1">
+            <div className="flex w-full p-1 ml-[12px]">
                 <img src={ChairGuy} alt="" className="lg:md:w-[54px] lg:md:h-[54px] w-[51px] h-[51px] rounded-full lg:md:mr-[13px] mr-[12px]" />
                 <div>
                     <h2 className="lg:md:text-[16px] text-[15px] font-medium lg:md:mb-[4px]">Name</h2>
@@ -52,7 +46,6 @@ const Contacts = () => {
             </div>
             <Divider variant="middle" className="shadow-sm lg:md:w-[100%] w-[90%]
                     lg:md:mb-[9.9px] mx-auto bg-[#f5ecec] lg:md:mt-[12px]" />
-
 
 
         </div>
